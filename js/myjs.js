9a5aa03008f352a82850375e7a840b8be159a823
QI <965297449@qq.com>
2023-06-05 23:50:18 +0800

    v3.完成指引页localstore的升级

--- js/myjs.js
+++ js/myjs.js
@@ -2,8 +2,8 @@
 
 const openModalBtn = document.getElementById('openModalBtn');
 const modal = document.querySelector('#myModal');
-console.log(modal);
-console.log(openModalBtn);
+// console.log(modal);
+// console.log(openModalBtn);
 
 // document.addEventListener('keydown', function(event) {
 //   if (event.key == p) { // Tab key code is 9
@@ -27,6 +27,57 @@ console.log(openModalBtn);
 //   modal.style.display = 'block';
     
 // });
+
+// 第二个模态窗口设置
+// 检查 localStorage 中是否存在 hasVisited 标志
+if (!localStorage.getItem('hasVisited')) {
+  // 如果不存在，则显示模态框
+  const modal2 = document.getElementById('my-modal2');
+   // 显示模态框
+modal2.style.display = 'block'; 
+
+  // 隐藏模态框的函数
+  function hideModal() {
+    modal2.style.display = 'none';
+  }
+  
+  // 给关闭按钮添加事件处理程序
+  const closeBtn = document.querySelector('#close-btn2');
+  console.log('关闭按钮',closeBtn);
+  closeBtn.onclick = function() {
+    console.log('点击了关闭');
+    hideModal();
+    // 将 hasVisited 标记为 true，表示已经看过提示内容
+    localStorage.setItem('hasVisited', true);
+  };
+ 
+}
+
+
+
+
+// 设置一个操作指引，打开页面之后，先询问你是不是，询问你需要帮助吗？ 
+
+// 放弃这种方式，每次都得询问
+  // if(confirm("你是QYC吗")){
+  //   alert('欢迎回来！')    
+  // } else{
+  //   alert('那你还是按一按回车键吧，就是Enter')
+  // }
+
+
+// 前提是按回车进入
+
+document.addEventListener('keydown',function(event){
+  if (event.key =='Enter'){
+      if (confirm('你想看一看指引吗')) {
+        window.location.href='https://github.com/QiYongchuan/Hope_Love/issues/2'
+      }
+      else{
+        alert('summer white mute pink black flowers quest dark 试一试首字母吧')
+      }
+  }
+})
 
 
 // 背景音乐播放
@@ -68,6 +119,14 @@ document.addEventListener("keydown",function(event){
     document.body.style.backgroundColor = 'white'
   }
   
+  if(event.key =='p'){
+    console.log('按下了p');
+    document.body.style.backgroundColor = 'pink'
+  }
+
+  if (event.key == 'g') {
+    document.body.style.backgroundColor = 'green'
+  }
 })
 
 // 监听hope和love，鼠标点击播放
@@ -156,27 +215,64 @@ btnqianxun.addEventListener('click',function(){
 
 const audioNowar = document.querySelector("#nowar")
 
-const btnNowar = document.querySelector('.btnNowar')
+const btnNowar = document.querySelectorAll('.btnNowar')
+
+for (let i = 0; i < btnNowar.length; i++) {
+  // 播放音乐
+  btnNowar[i].addEventListener('click',function(){
+    playAudioX(audioNowar)
+  })
+//  改变背景
+  btnNowar[i].addEventListener('click',function(){
+    playAudioX(audioNowar)
+  })
+}
 
 
-btnNowar.addEventListener('click',function(){
-  playAudioX(audioNowar)
-})
 
  // 点击某些图片实现更换背景颜色
 
-const star = document.querySelector('.star')
+const star = document.querySelectorAll('.star')
 
-star.addEventListener('click',function(){
-  document.body.style.backgroundColor = 'black'
-})
+for (let i = 0; i < star.length; i++) {
+  star[i].addEventListener('click', function() {
+   
+    document.body.style.backgroundColor = 'black';
+  });
+
+}
+// star.addEventListener('click',function(){
+//   document.body.style.backgroundColor = 'black'
+// })
 
 
-const pink = document.querySelector('.pink')
+const pink = document.querySelectorAll('.pink')
 
-pink.addEventListener('click',function(){
-  document.body.style.backgroundColor = 'pink'
-})
+console.log('pink',pink);
+
+for (let i = 0; i < pink.length; i++) {
+  pink[i].addEventListener('click', function() {
+    console.log("点击了 pink");
+    document.body.style.backgroundColor = 'pink';
+  });
+}
+
+// 红配绿太难看了，不把背景颜色改成绿色了
+// const green = document.querySelectorAll('.green')
+
+// for (let i = 0; i < green.length; i++) {
+//   green[i].addEventListener('click',function(){
+//     document.body.style.backgroundColor = '#71af29';
+//   })
+// }
+
+
+
+
+// pink.addEventListener('click',function(){
+//   console.log("点击了pink");
+//   document.body.style.backgroundColor = 'pink'
+// })
 
 
 
